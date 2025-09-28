36fc45341fcbb12df88034a8977abfeda3896a2b
wldasf <waleed.a.assaf@gmail.com>
2025-09-28 22:48:41 +0300

    Add group class

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from player import Player
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
-    pygame.init
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers =  (updatable, drawable)
+    Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while(True):
         for event in pygame.event.get():
@@ -19,10 +22,13 @@ def main():
 
         dt = clock.tick(60) / 1000.0
        
-        player.update(dt)
+        updatable.update(dt)
         
         screen.fill("black")
-        player.draw(screen)
+       
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
 
 if __name__ == "__main__":
