e31dd95cf87b50021a6f80a8b785c4503ba265e7
wldasf <waleed.a.assaf@gmail.com>
2025-10-04 14:50:15 +0300

    Split shot asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,3 +12,17 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            random_angle = random.uniform(20, 50)
+            new_vec1 = self.velocity.rotate(random_angle)
+            new_vec2 = self.velocity.rotate(-random_angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid_a = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid_a.velocity = new_vec1 * 1.2
+            asteroid_b = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid_b.velocity = new_vec2 * 1.2
--- main.py
+++ main.py
@@ -20,7 +20,6 @@ def main():
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable,)
     Shot.containers = (shots, updatable, drawable)
-    Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     AsteroidField()
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
@@ -39,11 +38,16 @@ def main():
                 import sys
                 sys.exit()
 
-        for asteroid in asteroids:
-            for shot in shots:
+        for asteroid in list(asteroids):
+            hit = False
+            for shot in list(shots):
                 if shot.collision(asteroid):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
+                    hit = True
+                    break
+            if hit:
+                continue
         
         screen.fill("black")
        
